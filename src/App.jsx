0296db3fcd9de7ff24ccec2b11fb7269bc349100
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2024-02-10 11:16:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
